460c275d9c168eb148ce08fd2d5437e14307b89c
Ernie Salazar <salazare@verizon.net>
2018-02-10 08:13:33 -0500

    disconnect unit tests

--- openfin/src-javascript/connection.test.js
+++ openfin/src-javascript/connection.test.js
@@ -17,21 +17,18 @@ describe('***** connection.js *****', () => {
         });
 
         it('connectPromise rejects when ws.CLOSED', (done) => {
-            setTimeout(() => done("timed out"), 500);
-
             mockWebSocket.send.resetHistory();
             mockWebSocket.readyState = ws.CLOSED;
 
             connection
-                .connectPromise(mockWebSocket)
+                .connectPromise()
                 .catch(() => {
                     assert.isTrue(mockWebSocket.send.notCalled);
                     done();
                 });
-        });
+        }).timeout(500);
 
         it('connectPromise still calls send when ws.OPEN', (done) => {
-            setTimeout(() => done("timed out"), 500);
             mockWebSocket.send.resetHistory();
 
             connection
@@ -40,21 +37,79 @@ describe('***** connection.js *****', () => {
                     assert.isTrue(mockWebSocket.send.calledOnce);
                     done();
                 });
-        });
+        }).timeout(500);
 
         it('connectPromise does not call send when session open is true', (done) => {
-            setTimeout(() => done("timed out"), 500);
             mockWebSocket.send.resetHistory();
-
             mockSessionStorage.getItem.reset();
             mockSessionStorage.getItem.returns('true');
 
             connection
-                .connectPromise(mockWebSocket)
+                .connectPromise()
                 .then(() => {
                     assert.isFalse(mockWebSocket.send.calledOnce);
                     done();
                 });
+        }).timeout(500);
+    });
+
+    describe('disconnectPromise', () => {
+        let connection;
+
+        beforeEach(() => {
+            connection = require('./connection').default;
+            connection.initialize(mockWebSocket, mockSessionStorage);
+            mockSessionStorage.getItem.returns("true");
+            mockWebSocket.readyState = ws.OPEN;
         });
+
+        it('disconnectPromise rejects when ws.CLOSED', (done) => {
+            mockWebSocket.send.resetHistory();
+            mockWebSocket.readyState = ws.CLOSED;
+
+            connection
+                .disconnectPromise()
+                .catch(() => {
+                    assert.isTrue(mockWebSocket.send.notCalled);
+                    done();
+                });
+        }).timeout(500);
+
+        it('disconnectPromise calls setItem', (done) => {
+            mockWebSocket.send.resetHistory();
+            mockWebSocket.readyState = ws.CLOSED;
+
+            connection
+                .disconnectPromise()
+                .catch(() => {
+                    assert.isTrue(mockSessionStorage.setItem.calledOnce);
+                    done();
+                });
+        }).timeout(500);
+
+
+        it('disconnectPromise does not call send when closed', (done) => {
+            mockWebSocket.send.resetHistory();
+            mockWebSocket.readyState = ws.CLOSED;
+
+            connection
+                .disconnectPromise()
+                .catch(() => {
+                    assert.isTrue(mockWebSocket.send.notCalled);
+                    done();
+                });
+        }).timeout(500);
+
+        it('disconnectPromise does not setItem when closed', (done) => {
+            mockSessionStorage.setItem.resetHistory();
+            mockWebSocket.readyState = ws.CLOSED;
+
+            connection
+                .disconnectPromise()
+                .catch(() => {
+                    assert.isTrue(mockSessionStorage.setItem.notCalled);
+                    done();
+                });
+        }).timeout(500);
     });
 });
